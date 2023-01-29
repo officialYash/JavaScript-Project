9cf743f159e65693a376c8802c44e08e6023ca03
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-29 11:30:33 +0530

    Password Generator Create Script File

---
+++ Password Generator Project/script.js
@@ -0,0 +1,32 @@
+const pwd = document.getElementById("pwd_txt");
+const generate = document.getElementById("generate");
+const clipboard = document.getElementById("clipboard");
+let pwd_length = document.getElementById("slider");
+let copty_text = document.getElementById("copy_text");
+let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklnopqrstuvwxyz0123456789";
+
+
+generate.addEventListener("click" , ()=>{
+    let password ="";
+    let checked = document.getElementById("checkbox").checked;
+    let final_string = string;
+    if(checked){
+        final_string += "@#$%^&*/=+?";
+    }
+    for (let i=0 ; i < pwd_length.value ; i++){
+        let pwd = final_string[Math.floor(Math.random() * final_string.length)];
+        password+=pwd;
+    }
+
+    pwd.innerText = password;
+    final_string = string;
+});
+
+clipboard.addEventListener("click" ,()=>{
+navigator.clipboard.writeText(pwd.innerText);
+copty_text.textContent ="Password Copied";
+copty_text.style.display="block";
+setTimeout(() =>{
+copty_text.style.display = "none";
+},2000);
+});
